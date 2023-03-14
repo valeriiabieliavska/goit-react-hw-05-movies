d9a70f8ec2a23c6abcbb0a69de573ada1ee83189
valeriiabieliavska <106553939+valeriiabieliavska@users.noreply.github.com>
2023-03-14 21:10:06 +0200

    react-router-dom

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { Routes } from "react-router-dom";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        
+      </Routes>
     </div>
   );
 };
